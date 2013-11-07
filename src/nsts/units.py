0a3ce8e1258744f81de8d47083a60df376d47b94
sque <sque@tolabaki.gr>
2013-11-07 19:18:37 +0200

    Units: Add support for parsing and arithmetic operations

--- src/nsts/units.py
+++ src/nsts/units.py
@@ -4,15 +4,29 @@ Created on Nov 5, 2013
 @author: Konstantinos Paliouras <sque '' tolabaki '' gr>
 '''
 
+import re
+
+class ParseError(RuntimeError):
+    '''
+    Error when parsing a unit
+    '''
+
 class Unit(object):
     '''
     Base class for defining a measurement unit
     '''
-    def __init__(self, name, initial_value, magnitudes):
-        
+    def __init__(self, name, initial_value, magnitudes, alt_magnitude_names = {}):
+        '''
+        @param name A friendly name describing the unit
+        @param initial_vale The initial value of this object
+        @param mangitudes A list of tuples with names and orders of magnitudes
+        @param alt_magnitude_names A dictionary of list with alternative names per magnitude
+        '''
+
         self.name = name
         self.raw_value = initial_value
         self.magnitudes = magnitudes
+        self.alt_magnitude_names = alt_magnitude_names
         self.magnitudes_map = {}
         self.default_magnitude = None
         
@@ -80,6 +94,25 @@ class Unit(object):
         if not scales:
             scales.append((value, self.magnitudes[0][1]))
         return scales
+    
+    def parse(self, string):
+        '''
+        Check objects value by parsing a string formatted expression
+        ''' 
+        pattern = re.compile('^\s*([0-9\.]+)\s*([^\s]+)\s*$')
+        match = pattern.match(string)
+        if not match:
+            raise ParseError("Error parsing '{0}'".format(string))
+        
+        quantity = float(match.group(1))
+        unit_type = match.group(2)
+        for magnitude,order in self.magnitudes_map.iteritems():
+            if unit_type == magnitude or unit_type in self.alt_magnitude_names[magnitude]:
+                self.raw_value = order * quantity
+                return # Found
+                
+        
+        raise ParseError("Error parsing '{0}'".format(string))
 
     def optimal_scale_str(self):
         '''
@@ -103,6 +136,41 @@ class Unit(object):
     def __repr__(self):
         return "{0}({1})".format(self.name, self.__str__())
     
+    def __eq__(self, other):
+        assert type(self) == type(other)
+        return self.raw_value == other.raw_value
+
+    def __ne__(self, other):
+        assert type(self) == type(other)
+        return self.raw_value != other.raw_value
+    
+    def __lt__(self, other):
+        assert type(self) == type(other)
+        return self.raw_value < other.raw_value
+    
+    def __le__(self, other):
+        assert type(self) == type(other)
+        return self.raw_value <= other.raw_value
+    
+    def __gt__(self, other):
+        assert type(self) == type(other)
+        return self.raw_value > other.raw_value
+    
+    def __ge__(self, other):
+        assert type(self) == type(other)
+        return self.raw_value >= other.raw_value
+    
+    def __nonzero__(self):
+        return bool(self.raw_value)
+    
+    def __add__(self, other):
+        assert type(self) == type(other)
+        return type(self)(self.raw_value + other.raw_value)
+    
+    def __sub__(self, other):
+        assert type(self) == type(other)
+        return type(self)(self.raw_value - other.raw_value)
+    
 class BitRateUnit(Unit):
     '''
     BitRate measurement unit
@@ -114,7 +182,16 @@ class BitRateUnit(Unit):
                       (10**6, 'Mbit/s'),
                       (10**9, 'Gbit/s'),
                       (10**11,'Tbit/s')]
-        super(BitRateUnit, self).__init__("Transfer Rate", initial_value, magnitudes)
+        
+        alt_magnitude_names = {
+                'bits/s' : ['b/s', 'bps'],
+                'kbit/s' : ['Kbit/s', 'Kbits/s', 'Kb/s', 'kbps'],
+                'Mbit/s' : ['Mbits/s', 'Mb/s', 'Mbps'],
+                'Gbit/s' : ['Gbits/s', 'Gb/s', 'Gbps'],
+                'Tbit/s' : ['Tbits/s', 'Tb/s', 'Tbps'],
+                }
+        
+        super(BitRateUnit, self).__init__("Transfer Rate", initial_value, magnitudes, alt_magnitude_names)
  
 
 class TimeUnit(Unit):
@@ -160,7 +237,7 @@ if __name__ == '__main__':
              TimeUnit(0.020),
              TimeUnit(0.0001)
              ]
-    print " Optimal Scale"
+    print "Optimal Scale"
     # Time
     for time in times:
         print "Time| raw: {0}, optimal : {1}, combined: {2}, str: '{3}'".format(
@@ -191,3 +268,63 @@ if __name__ == '__main__':
     print PacketUnit()
     print PacketUnit(12)
     print PacketUnit(14)
+    
+    print "UNIT TESTING"
+    print "------------------------------------------"
+    print "Parsing"
+    bit_tests = [
+                ("1bps", BitRateUnit(1)),
+                ("40 bps", BitRateUnit(40)),
+                ("47 bits/s", BitRateUnit(47)),
+                ("12.67 Gbits/s", BitRateUnit(12.67*1000*1000*1000)),
+                ("12.32 Gb/s", BitRateUnit(12.32*1000*1000*1000)),
+                ("12.543 Gbps", BitRateUnit(12.543*1000*1000*1000))
+            ]
+    
+    for btest in bit_tests:
+        b = BitRateUnit()
+        b.parse(btest[0])
+        assert b == btest[1]
+        print btest[0], b, btest[1] 
+    
+    try:
+        b.parse("12.543 dummy")
+    except:
+        print "Caught exception as expected"
+        
+    
+    print "\nComparisons"
+    assert not BitRateUnit(0)
+    assert BitRateUnit(1)
+    
+    assert not BitRateUnit(1) == BitRateUnit(0)
+    assert BitRateUnit(1) != BitRateUnit(0)
+    assert not BitRateUnit(1) < BitRateUnit(0)
+    assert not BitRateUnit(1) <= BitRateUnit(0)
+    assert BitRateUnit(1) > BitRateUnit(0)
+    assert BitRateUnit(1) >= BitRateUnit(0)
+    
+    assert BitRateUnit(1) == BitRateUnit(1)
+    assert not BitRateUnit(1) != BitRateUnit(1)
+    assert not BitRateUnit(1) < BitRateUnit(1)
+    assert BitRateUnit(1) <= BitRateUnit(1)
+    assert not BitRateUnit(1) > BitRateUnit(1)
+    assert BitRateUnit(1) >= BitRateUnit(1)
+    
+    assert not BitRateUnit(0) == BitRateUnit(1)
+    assert BitRateUnit(0) != BitRateUnit(1)
+    assert BitRateUnit(0) < BitRateUnit(1)
+    assert BitRateUnit(0) <= BitRateUnit(1)
+    assert not BitRateUnit(0) > BitRateUnit(1)
+    assert not BitRateUnit(0) >= BitRateUnit(1)
+    print "Passed."
+
+    print "\nArithmetic operations"
+    a = BitRateUnit(4.5)
+    b = BitRateUnit(1.2)
+    c = BitRateUnit(5.7)
+    print "a + b = c => {0} + {1} = {2}".format(a, b, a+b)
+    assert a+b == c
+    c = BitRateUnit(3.3)
+    print "a - b = c => {0} - {1} = {2}".format(a, b, a-c)
+    assert a-b == c
